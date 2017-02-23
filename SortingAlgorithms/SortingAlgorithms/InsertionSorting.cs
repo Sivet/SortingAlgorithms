f54bd782ce4b0d8d4b95e3cea34287d6348da6e8
simon <simon@glashoff.dk>
2017-02-23 11:13:46 +0100

    Insert sorting implementeret

---
+++ SortingAlgorithms/SortingAlgorithms/InsertionSorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class InsertionSorting
+    {
+        public int[] Sort(int[] notSorted)
+        {
+            int temp, index;
+            for (int i = 1; i < notSorted.Length; i++)
+            {
+                temp = notSorted[i];
+                index = i - 1;
+
+                while (index >= 0 && notSorted[index] > temp)
+                {
+                    notSorted[index + 1] = notSorted[index];
+                    index--;
+                }
+
+                notSorted[index + 1] = temp;
+            }
+            return notSorted;
+        }
+    }
+}
---
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            InsertionSorting Insertion = new InsertionSorting();
+            
+            int[] notSorted = new int[10];
+            int[] Sorted = new int[10];
+
+            for (int i = 0; i < notSorted.Length; i++)
+            {
+                int value = random.Next(1, 5000);
+                notSorted[i] = value;
+            }
+            Insertion.Sort(notSorted);
+
+            Console.WriteLine("==========Integer Array OutPut===============");
+            for (int i = 0; i < notSorted.Length; i++)
+            {
+                Console.WriteLine(notSorted[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
